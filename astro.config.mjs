3f26bdf435c07b745d631c3039fc0f55fe5d74f0
Adnen Rebai <adnen.rebai@gmail.com>
2026-02-25 05:32:20 +0100

    feat : update paths in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,67 @@ export default defineConfig({
           label: "PHP Book",
           autogenerate: { directory: "php-book" },
         },
+        {
+          label: "PHP Fondamental",
+          autogenerate: { directory: "php-fondamental" },
+        },
+        {
+          label: "Control Structures",
+          autogenerate: { directory: "control-structures" },
+        },
+        {
+          label: "Functions",
+          autogenerate: { directory: "Functions" },
+        },
+        {
+          label: "Arrays",
+          autogenerate: { directory: "Arrays" },
+        },
+        {
+          label: "OOP in PHP",
+          autogenerate: { directory: "Object-Oriented-Programming" },
+        },
+        {
+          label: "Advanced OOP",
+          autogenerate: { directory: "Advanced-OOP-Concepts" },
+        },
+
+        {
+          label: "Error and Exceptions",
+          autogenerate: { directory: "Error-and-Exceptions-Handling" },
+        },
+        {
+          label: "Namespaces",
+          autogenerate: { directory: "Namespaces" },
+        },
+        {
+          label: "File and Directories",
+          autogenerate: { directory: "Working-with-File-and-Directories" },
+        },
+        {
+          label: "String Manipulation",
+          autogenerate: { directory: "String-Manipulation" },
+        },
+        {
+          label: "Date and Time",
+          autogenerate: { directory: "Date-and-Time" },
+        },
+        {
+          label: "Web dev with PHP",
+          autogenerate: { directory: "Web-dev-with-PHP" },
+        },
+        {
+          label: "Database Integration",
+          autogenerate: { directory: "Database-Integration" },
+        },
+        {
+          label: "Security",
+          autogenerate: { directory: "Security" },
+        },
+        {
+          label: "Performance and Optimization",
+          autogenerate: { directory: "Performance-and-Optimization" },
+        },
       ],
       customCss: ["./src/styles/global.css"],
     }),
